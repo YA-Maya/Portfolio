4abf74e214124605aa98b4b553a28eec5bcc7ac7
Sumeya <98185508+YA-Maya@users.noreply.github.com>
2023-02-13 17:40:08 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+/*==Menu show Y hidden=== */
+const navMenu= document.getElementById('nav-menu'),
+toggleMenu = document.getElementById('nav-toggle'),
+closeMenu= document.getElementById('nav-close')
+
+
+//show
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+//hidden
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+/*==remove menu ====*/
+const navLink= document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n=> n.addEventListener('click',linkAction ))
+/*==SCROLL sections active link=== */
+const sections= document.querySelectorAll('section[id]')
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY= window.pageYOffset
+
+    sections.forEach(current=>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionsId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__manu a[href*='+ sectionId + ']').classList.add('active')
+        } else{
+            document.querySelector('.nav__manu a[href*='+ sectionId + ']').classList.remove('active')
+
+        }
+    })
+}
